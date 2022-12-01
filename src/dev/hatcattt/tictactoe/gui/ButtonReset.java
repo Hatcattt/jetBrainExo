903f5db4c0ab15fcadc4ed1d880b901f3e6f16cd
Hatcattt <hatcattt.dev@gmail.com>
2022-12-01 11:03:56 +0100

    add logger

--- src/dev/hatcattt/tictactoe/gui/ButtonReset.java
+++ src/dev/hatcattt/tictactoe/gui/ButtonReset.java
@@ -3,9 +3,6 @@ package dev.hatcattt.tictactoe.gui;
 import dev.hatcattt.tictactoe.main.Board;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,6 +13,9 @@ public class ButtonReset extends JButton {
     private final static Logger LOGGER = Logger.getLogger(ButtonReset.class.getName());
     private final String name = "Reset";
 
+    /**
+     * Constructor of the rest button.
+     */
     public ButtonReset() {
         super.setName(name);
         super.setText(name);
