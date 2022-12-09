e6d6b2a302f5fa5758cacfc14b585a4e5eccc4a7
Maxence Mercier <71705747+Hatcattt@users.noreply.github.com>
2022-12-09 21:28:37 +0100

    change this folder to a real gihtub repo

--- src/dev/hatcattt/tictactoe/GameRun.java
+++
@@ -1,9 +0,0 @@
-package dev.hatcattt.tictactoe;
-
-import dev.hatcattt.tictactoe.main.TicTacToe;
-
-public class GameRun {
-    public static void main(String[] args) {
-        new TicTacToe();
-    }
-}
